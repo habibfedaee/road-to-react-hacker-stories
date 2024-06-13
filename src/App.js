e8c1c49f7162ad0161008e5c645d3b12f697f711
habibfedaee <habib.fedaee@gmail.com>
2024-06-12 20:33:37 -0700

    Embedded JS in JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 
 function App() {
+  function getTitle(title) {
+    return title;
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle("reactttt!")}</h1>
+
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
