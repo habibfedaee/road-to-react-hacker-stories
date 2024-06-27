f37663135b297de7228eec997da1a2e4633365f7
habibfedaee <habib.fedaee@gmail.com>
2024-06-27 11:59:14 -0700

    props added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ function List(props) {
   return (
     <ul>
       {props.list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title} </a>
-            </span>
-            <span>{item.author} </span>
-            <span>{item.num_comments} </span>
-            <span>{item.points} </span>
-          </li>
-        );
+        return <Item item={item} />;
       })}
     </ul>
   );
 }
+const Item = (props) => {
+  const item = props.item;
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title} </a>
+      </span>
+      <span>{item.author} </span>
+      <span>{item.num_comments} </span>
+      <span>{item.points} </span>
+    </li>
+  );
+};
 function Search() {
   const handleChange = (event) => {
     console.log(event);
